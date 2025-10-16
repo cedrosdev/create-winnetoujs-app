faf0c697a7b0b769d6e99e1eb0ee21830e69bf93
Pamela <pamela@cedros.dev>
2025-10-16 15:16:09 -0300

    1.1.1 - fixed ssr start script

--- templates/node-ssr-ts/View/winnetou-ssr/src/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.escapeHTML = exports.joinConstructos = exports.$html = void 0;
-var ssr_1 = require("winnetoujs/modules/ssr");
-Object.defineProperty(exports, "joinConstructos", { enumerable: true, get: function () { return ssr_1.joinConstructos; } });
-Object.defineProperty(exports, "escapeHTML", { enumerable: true, get: function () { return ssr_1.escapeHTML; } });
-var home_wcto_1 = require("./home/home.wcto");
-Object.defineProperty(exports, "$html", { enumerable: true, get: function () { return home_wcto_1.$html; } });
