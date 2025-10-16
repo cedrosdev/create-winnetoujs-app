0c1cf61ca4c6754c04d22851a9ef0478e2bcf0f9
Pamela <pamela@cedros.dev>
2025-10-16 16:06:48 -0300

    1.1.3 - remove vscode workspace file

--- templates/node-ssr-ts/Server-dist/Controller/api.d.ts
+++
@@ -1,3 +0,0 @@
-import { Express } from "express";
-export declare const api: (app: Express) => void;
-//# sourceMappingURL=api.d.ts.map
--- templates/node-ssr-ts/Server-dist/Model/api.model.d.ts
+++
@@ -1,5 +0,0 @@
-declare const healthCheck: () => {
-    status: string;
-};
-export { healthCheck };
-//# sourceMappingURL=api.model.d.ts.map
